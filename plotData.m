9dffc5571f196f58df2265444b1960cbd21ffae7
yjessicaz <53984449+yjessicaz@users.noreply.github.com>
2019-11-19 21:16:27 -0800

    Update plotData.m

--- plotData.m
+++ plotData.m
@@ -18,8 +18,8 @@ figure; % open a new figure window
 
 
 plot(x,y,'rx','MarkerSize',10);
-ylabel('Profit in $10,000s');
-xlabel('Population of City in 10,000s');
+ylabel('LABEL YOUR Y AXIS');
+xlabel('HURRY AND LABEL YOUR POOR X AXIS');
 
 
 % ============================================================
